83ae5c03da03648e8e2157c98d73893437a89c90
FlopyFlop <yoav1409@gmail.com>
2021-01-18 19:01:57 +0200

    Fixing script to put airports as lower case

--- scripts/add_flights.py
+++ scripts/add_flights.py
@@ -8,7 +8,7 @@ app_context.push()
 departure = datetime.datetime(year=2021, month=1, day=1, hour=17, minute=0, second=0)
 landing = departure + datetime.timedelta(hours=5)
 for i in range(1, 30):
-    flight_added = Flight(source="London", destination="New York",
+    flight_added = Flight(source="london", destination="new york",
                             landing=landing, departure=departure, capacity=300)
     db.session.add(flight_added)
     departure = departure + datetime.timedelta(days=2)
